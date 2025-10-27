67a363417c67bf4d87960cdd3e9b2da51fc9a953
Michael Ilyin <milyin@gmail.com>
2025-10-27 21:07:36 +0100

    with-menu functions

--- yoroolbot/src/api/command_trait/mod.rs
+++ yoroolbot/src/api/command_trait/mod.rs
@@ -50,24 +50,75 @@ impl CommandReplyTarget {
             .markdown_message(self.chat.id, self.msg_id, text)
             .await?;
 
-        // Pack callback data and attach keyboard to the message
-        let keyboard = pack_callback_data(
+        Self::attach_menu_to_message(
+            &self.bot,
             &self.callback_data_storage,
             self.chat.id,
-            msg.id.0,
+            msg.id,
             menu,
         )
-        .await;
-        self.bot
-            .edit_message_reply_markup(self.chat.id, msg.id)
-            .reply_markup(keyboard)
-            .await?;
+        .await?;
 
         Ok(msg)
     }
 
     pub fn send_markdown_message(&self, text: MarkdownString) -> JsonRequest<SendMessage> {
         self.bot.send_markdown_message(self.chat.id, text)
+    }
+
+    /// Send a markdown message with an inline keyboard menu using a request builder
+    /// The menu is automatically packed using pack_callback_data to handle long callback data
+    pub async fn send_markdown_message_with_menu<R, B>(
+        &self,
+        text: MarkdownString,
+        menu: impl IntoIterator<Item = R>,
+    ) -> ResponseResult<Message>
+    where
+        R: IntoIterator<Item = B>,
+        B: Into<(String, String)>,
+    {
+        let msg = self
+            .bot
+            .send_markdown_message(self.chat.id, text)
+            .await?;
+
+        Self::attach_menu_to_message(
+            &self.bot,
+            &self.callback_data_storage,
+            self.chat.id,
+            msg.id,
+            menu,
+        )
+        .await?;
+
+        Ok(msg)
+    }
+
+    /// Helper function to attach a menu to an existing message
+    /// Extracted to avoid code duplication between different send methods
+    async fn attach_menu_to_message<R, B>(
+        bot: &Bot,
+        callback_data_storage: &Arc<dyn CallbackDataStorageTrait>,
+        chat_id: teloxide::types::ChatId,
+        message_id: MessageId,
+        menu: impl IntoIterator<Item = R>,
+    ) -> ResponseResult<()>
+    where
+        R: IntoIterator<Item = B>,
+        B: Into<(String, String)>,
+    {
+        // Pack callback data and attach keyboard to the message
+        let keyboard = pack_callback_data(
+            callback_data_storage,
+            chat_id,
+            message_id.0,
+            menu,
+        )
+        .await;
+        bot.edit_message_reply_markup(chat_id, message_id)
+            .reply_markup(keyboard)
+            .await?;
+        Ok(())
     }
 
     pub fn edit_markdown_message_text(
